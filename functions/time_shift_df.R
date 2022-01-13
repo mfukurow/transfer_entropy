2197cf201094f2fd0fc7b50f20520b622fb50c1d
mfukurow <mfukurow@gmail.com>
2022-01-13 11:03:46 -0600

    separate the function file (time_shift_df.R)

---
+++ functions/time_shift_df.R
@@ -0,0 +1,48 @@
+
+##-------------------------------------------------------------------------
+##
+##  time_shift_df.R
+##    creates a data frame including columns that is time-shifted up to the 
+##    number specified by tau.
+##
+##  Author        : Matasaburo Fukutomi
+##  First publish : 01.12.2021
+##  Last update   : 01.12.2021
+##
+##  Usage
+##    tsdf <- time_shift_df(df, tau)
+##
+##  Variables
+##    inputs
+##      df = data frame containing variables
+##      tau = number of previous time points
+##    output
+##      tsdf = time-shifted data frame
+##
+##-------------------------------------------------------------------------
+
+time_shift_df <- function(df, tau) {
+  
+  # gather df structure information
+  n_col <- ncol(df)
+  n_row <- nrow(df)
+  Vnames <- names(df)
+  
+  # determine tsdf structure
+  n_col_tsdf <- n_col * (tau + 1)
+  n_row_tsdf <- n_row - tau
+  
+  # loop for making time-shifted data
+  tsdf <- matrix(numeric(n_col_tsdf * n_row_tsdf), ncol = n_col_tsdf)
+  for (ii in 1:n_col) {
+    for (jj in 0:tau) {
+      tsdf[, (ii - 1) * (tau + 1) + jj + 1] <- df[(jj + 1):(n_row - tau + jj), ii]
+    }
+  }
+  
+  # output
+  tsdf <- data.frame(tsdf)
+  names(tsdf) <- paste0(rep(Vnames, each = tau + 1), 0:tau)
+  return(tsdf)
+  
+}
