c74117bc673ea26f826b83c33f26d37340a451bd
mfukurow <mfukurow@gmail.com>
2022-01-13 15:30:30 -0600

    build a function transfer_entropy

--- functions/transfer_entropy.R
+++ functions/transfer_entropy.R
@@ -6,7 +6,7 @@
 ##
 ##  Author        : Matasaburo Fukutomi
 ##  First publish : 01.12.2021
-##  Last update   : 01.12.2021
+##  Last update   : 01.13.2021
 ##
 ##  Usage
 ##    TE <- transfer_entropy(df, tau, breaks_list)
@@ -14,6 +14,8 @@
 ##  Variables
 ##    inputs
 ##      df = a data frame containing variables
+##           includes only two columns; 
+##           the first is "effect" and the second is "cause"
 ##      tau = number of previous time points for the calculation
 ##      list_breaks = bins for each variable in a list format
 ##    output
@@ -22,30 +24,22 @@
 ##-------------------------------------------------------------------------
 
 
-# Functions ---------------------------------------------------------------
+# Function ---------------------------------------------------------------
 
-time_shift_df <- function(df, tau) {
+transfer_entropy <- function(df, tau, list_breaks) {
+  # 1. make a time-shifted data frame
+  tsdf <- time_shift_df(df, tau)
+  list_breaks <- rep(list_breaks, each = tau + 1)
+  Vnames <- names(tsdf)
+  names(list_breaks) <- Vnames
   
-  # gather df structure information
-  n_col <- ncol(df)
-  n_row <- nrow(df)
-  Vnames <- names(df)
+  # 2. calculate transfer entropy
+  TE <- joint_entropy_H(tsdf[, 1:(tau + 1)], list_breaks[Vnames[1:(tau + 1)]]) -
+    joint_entropy_H(tsdf[, 2:(tau + 1)], list_breaks[Vnames[2:(tau + 1)]]) -
+    joint_entropy_H(tsdf[, c(1:(tau + 1), (tau + 3):(2 * (tau + 1)))], 
+                    list_breaks[Vnames[c(1:(tau + 1), (tau + 3):(2 * (tau + 1)))]]) +
+    joint_entropy_H(tsdf[, c(2:(tau + 1), (tau + 3):(2 * (tau + 1)))],
+                    list_breaks[Vnames[c(2:(tau + 1), (tau + 3):(2 * (tau + 1)))]])
   
-  # determine tsdf structure
-  n_col_tsdf <- n_col * (tau + 1)
-  n_row_tsdf <- n_row - tau
-  
-  # loop for making time-shifted data
-  tsdf <- matrix(numeric(n_col_tsdf * n_row_tsdf), ncol = n_col_tsdf)
-  for (ii in 1:n_col) {
-    for (jj in 0:tau) {
-      tsdf[, (ii - 1) * (tau + 1) + jj + 1] <- df[(jj + 1):(n_row - tau + jj), ii]
-    }
-  }
-  
-  # output
-  tsdf <- data.frame(tsdf)
-  names(tsdf) <- paste0(rep(Vnames, each = tau + 1), 0:tau)
-  return(tsdf)
-  
+  return(TE)
 }
